bd1ce3379a397185388695c288efcc1525e2d1f5
Alessandro Mento <ale.mento2012@gmail.com>
2022-02-23 00:48:20 +0100

    Solve problem #2

--- problems/problem-1.kt
+++ problems/problem-1.kt
@@ -1,5 +1,20 @@
 package problems
 
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
 fun main() {
-    println("Hello")
+
+    var sum = 0
+
+    for (i in 3..999) {
+
+        if ((i.mod(3) == 0) || (i.mod(5) == 0))
+            sum += i
+    }
+
+    println("Sum is $sum")
 }
