f8f210fdde51c60431df5748de6c1cbe12bdeebb
Alessandro Mento <ale.mento2012@gmail.com>
2022-02-23 19:24:23 +0100

    Solve problem #4

--- problems/problem-4.kt
+++ problems/problem-4.kt
@@ -1,5 +1,7 @@
 package problems
 
+import kotlin.math.min
+
 /**
  * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
  *
@@ -7,27 +9,37 @@ package problems
  */
 fun main() {
 
-    val largestPalindrome= largestPalindrome()
+    val largestPalindrome= largestPalindrome(3)
     println("Largest palindrome: $largestPalindrome")
 }
 
-fun largestPalindrome() : Int {
+fun largestPalindrome(numbersDigits: Int) : Int {
+
+    val minNumberStringBuilder: StringBuilder = java.lang.StringBuilder("1")
+    val maxNumberStringBuilder: StringBuilder = java.lang.StringBuilder("9")
+
+    for (i in 1 until numbersDigits) {
+        minNumberStringBuilder.append("0")
+        maxNumberStringBuilder.append("9")
+    }
+    val minNumber = minNumberStringBuilder.toString().toInt()
+    val maxNumber = maxNumberStringBuilder.toString().toInt()
 
     var largest = 0
 
-    for (i in 999 downTo 100) {
+    for (i in maxNumber downTo minNumber) {
 
-        for (j in 999 downTo 100) {
+        for (j in maxNumber downTo minNumber) {
 
             val product = (i*j)
             val productString = product.toString()
             val productLength = productString.length
 
-            for (k in 0..2) {
+            for (k in 0 until numbersDigits) {
 
                 if (productString[k] != productString[(productLength-1)-k]) break
 
-                if (k==2) // Passed palindrome check above
+                if (k==(numbersDigits-1)) // Passed palindrome check above
                     if (product > largest)
                         largest = product
             }
